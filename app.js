eca6cde3958b4f7d10887a058a5897a3095bf031
chavangorakh1999 <chavangorakh19999@ggmail.com>
2020-11-03 01:09:29 +0530

    adding level 2 security to sign in process

--- app.js
+++ app.js
@@ -1,2 +1,81 @@
 //jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+
+
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields:["password"]});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.username }, (err, foundUser) => {
+    if (!err) {
+      if (foundUser) {
+          if(foundUser.password === req.body.password){
+            res.render("secrets");
+          }else{
+              console.log("Wrong password");
+          }
+      } else {
+        console.log("No user Found please register");
+      }
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (req, res) => {
+  console.log("Sucessfuly started at port " + port);
+});
